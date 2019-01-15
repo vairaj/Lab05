461bac922a3b03451c79e37caebbcc7c11564643
vairaj <43154018+vairaj@users.noreply.github.com>
2019-01-15 00:26:56 -0700

    Add files via upload

--- Lab5MyStack.cpp
+++ Lab5MyStack.cpp
@@ -1,3 +1,30 @@
 //Jasmine Vaira
+//COSC 2030 FALL 2018
+//LAB 5
 
 #include "stdafx.h"
+#include "Lab5MyStack.h"
+
+bool MyStack::isEmpty() const
+{
+	if (MyStack::data.empty() == true)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void MyStack::push(char& c)
+{
+	MyStack::data.push_back(c);
+}
+
+char MyStack::pull()
+{
+	char x = MyStack::data.back();
+	MyStack::data.pop_back();
+	return x;
+}
--- Lab5MyStack.h
+++ Lab5MyStack.h
@@ -1,3 +1,18 @@
+//Jasmine Vaira
+//COSC 2030 FALL 2018
+// LAB 5
 #pragma once
 #include "stdafx.h"
-//Jasmine Vaira
+#include <vector>
+#include <list>
+using namespace std;
+
+class MyStack
+{
+	//vector<char> data;
+	list<char> data;
+public:
+	bool isEmpty() const;
+	void push(char& c);
+	char pull();
+};
